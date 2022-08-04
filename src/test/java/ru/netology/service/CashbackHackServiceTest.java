892921836aa27c90c53aa47e27f138e5fdd6b739
ElenaPonomareva <ponomareva@internet.ru>
2022-08-04 14:39:33 +0500

    add junit jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -14,8 +15,6 @@ public class CashbackHackServiceTest {
         int expected = 700;
 
         Assert.assertEquals(expected, actual);
-
-
     }
 
     @Test
@@ -36,6 +35,37 @@ public class CashbackHackServiceTest {
         int expected = 0;
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2IfAmountMore1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(5300);
+        int expected = 700;
+
+        Assertions.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void test2IfAmountLessThan1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(550);
+        int expected = 450;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2IfAmountEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(2000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
     }
 
 }
